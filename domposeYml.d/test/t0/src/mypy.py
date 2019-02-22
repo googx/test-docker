a56905bcbac4a1e09043809c7cf1510143d1fbb2
hx940929 <836845967@qq.com>
2019-02-22 22:22:31 +0800

    pkg-resource使用.

--- domposeYml.d/test/t0/src/mypy.py
+++ domposeYml.d/test/t0/src/mypy.py
@@ -1,19 +1,31 @@
-#!/usr/bin/python3
-# coding=utf-8
-# --------------------------------------------------
-# File Name: 
-# Author: hanxu
-# AuthorSite: https://www.thesunboy.com/
-# Created Time: 2018-07-25 上午10:23
-# ---------------------说明--------------------------
-#  
-# ---------------------------------------------------
+#!/usr/bin/python
+# EASY-INSTALL-ENTRY-SCRIPT: 'docker-compose==1.8.0','console_scripts','docker-compose'
+# __requires__ = 'docker-compose==1.8.0'
+import pkg_resources
+import psutil
 
-import os;
+# def ss(ss:pkg_resources.EntryPoint):
+# ss.
+# pass
 
-osname = "名称:" + os.getenv("test");
-print("系统" + osname + os.name);
-print("环境变量test" + osname + os.getenv("test"));
-print("环境变量test2" + osname + os.getenv("test2"));
-print("环境变量test3" + osname + os.getenv("test3"));
-print("不存在的环境变量：" + os.getenv("test111"));
+if __name__ == '__main__':
+    # print(psutil.users())
+    pa = psutil.Process()
+    psdir = dir(psutil)
+    # print(psutil.users())
+    requ = pkg_resources.parse_requirements(psutil)
+    print(type(requ))
+    requ()
+    # requ.
+    # for i in requ:
+    #     print(i)
+    info = pkg_resources.get_entry_info('docker-compose==1.8.0', 'console_scripts', 'docker-compose')
+    # info1=pkg_resources.get_entry_info('psutil==5.4.8', 'console_scripts', 'users')
+    # print(info.name)
+    # print(info.attrs)
+    # print(info.dist)
+    # print(info.extras)
+    # ss(info)
+    # sys.exit(
+    #     load_entry_point('docker-compose==1.8.0', 'console_scripts', 'docker-compose')()
+    # )
